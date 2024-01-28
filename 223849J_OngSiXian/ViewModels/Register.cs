f957655ecc3ec182fdc234b5b453e5d158b4e3e8
ONG SI XIAN <223849J@mymail.nyp.edu.sg>
2024-01-29 01:15:25 +0800

    Ensure password length is min 12 instead of 8

--- 223849J_OngSiXian/ViewModels/Register.cs
+++ 223849J_OngSiXian/ViewModels/Register.cs
@@ -45,7 +45,7 @@ namespace _223849J_OngSiXian.ViewModels
 		[Required]
 		[DataType(DataType.Password)]
 		[MinLength(12, ErrorMessage = "Enter at least a 12 characters password")]
-		[RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$",
+		[RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{12,}$",
 		ErrorMessage = "Passwords must be at least 12 characters long and contain at least an upper case letter, lower case letter, digit and a symbol")]
 		public string Password { get; set; }
 
